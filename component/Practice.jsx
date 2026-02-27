9bf190e6b5ef41039b51e54be7757cec339ff67c
Shivani332 <shivaninamdev332@gmail.com>
2026-02-27 15:51:51 +0530

    update practice

--- component/Practice.jsx
+++ component/Practice.jsx
@@ -6,6 +6,7 @@ const Practice = () => {
         <h1>this is practice for git </h1>
         <h2>now i learn add main into practice</h2>
         <h3>now i practice git  </h3>
+        <h4>i am in practice branch</h4>
     </div>
   )
 }
